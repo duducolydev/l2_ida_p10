0dfbdbb053f8686a55ab2a4afbacdb7c03c72191
Doudou COLY <dcoly@ansd.local>
2025-07-05 15:02:29 +0000

    test exception

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
-
-echo readfile("test_sessions.php");
-
+function checkNum($number)
+{ //Fonction avec une exception
+    if ($number < 0) {
+        throw new Exception("Le nombre $number n'est pas un nombre positif !");
+    }
+    return true;
+}
+try { //Declencher une exception dans un bloc "try"
+    checkNum(-10);
+    // Si l'exception est lancée, ce texte ne sera pas affiché
+    echo "Bravo ! le nombre choisi est bien une valeur positive !";
+} catch (Exception $e) { //Capturer une exception
+    echo 'Message: ' . $e->getMessage();
+}
